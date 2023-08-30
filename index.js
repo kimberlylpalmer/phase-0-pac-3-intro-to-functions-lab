fc5da662ea3f717dfb0a7fbe2972aadfac34383d
Kimberly L Palmer <kimberlylpalmer@gmail.com>
2023-08-30 16:51:45 -0700

    Updated index.js file per lesson

---
+++ index.js
@@ -0,0 +1,31 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+function logShout(string) {
+    console.log(string.toUpperCase());
+}
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toLowerCase()) {
+        // Block of code
+        return "I can't hear you!"
+     
+      } 
+      if (string === string.toUpperCase()) {
+        // Block of code
+        return "YES INDEED!"
+     
+      } 
+      if (string === "Let's have dinner together!") {
+        // Block of code
+        return "I would love to!"
+     
+      } 
+}
